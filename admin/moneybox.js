45ccb6c6081a4c6cb9252585d0680b7e1b889f35
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-05-23 15:49:31 +0200

    add defer to admin scripts

--- admin/moneybox.js
+++ admin/moneybox.js
@@ -1,2 +1,23 @@
-console.log("Moneybox connected");
+/* =========================
+   MONEYBOX ELEMENTS
+========================= */
+
+const moneyboxAmount =
+  document.getElementById(
+    "moneybox-amount"
+  );
+
+const moneyboxCheckbox =
+  document.getElementById(
+    "moneybox-checkbox"
+  );
+
+const moneyboxTotal =
+  document.getElementById(
+    "moneybox-total"
+  );
+
+console.log(moneyboxAmount);
+console.log(moneyboxCheckbox);
+console.log(moneyboxTotal);
 
