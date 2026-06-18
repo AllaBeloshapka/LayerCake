12cbbe4357523f24ee44693dcfa192724aefc19e
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-06-18 11:28:04 +0200

    Return test products from products route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,32 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "Products route is working",
-  });
+  res.json([
+    {
+      id: 1,
+      name: "Napoleon Cake",
+      price: 1200,
+      category: "Cakes",
+      image: "/images/napoleon.jpg",
+      description: "Classic layered cake with custard cream",
+    },
+    {
+      id: 2,
+      name: "Honey Cake",
+      price: 950,
+      category: "Cakes",
+      image: "/images/medovik.jpg",
+      description: "Honey sponge layers with smooth sour cream filling",
+    },
+    {
+      id: 3,
+      name: "Red Velvet Cake",
+      price: 1500,
+      category: "Cakes",
+      image: "/images/red-velvet.jpg",
+      description: "Chocolate sponge with cream cheese frosting and berry sauce",
+    },
+  ]);
 });
 
 module.exports = router;
