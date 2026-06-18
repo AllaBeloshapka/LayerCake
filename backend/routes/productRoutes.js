c4a3877d402d9cead890383befd5dff219b7249c
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-06-18 11:42:25 +0200

    Move test products to data file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,10 @@
 const express = require("express");
+const testProducts = require("../data/testProducts");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: "Napoleon Cake",
-      price: 1200,
-      category: "Cakes",
-      image: "/images/napoleon.jpg",
-      description: "Classic layered cake with custard cream",
-    },
-    {
-      id: 2,
-      name: "Honey Cake",
-      price: 950,
-      category: "Cakes",
-      image: "/images/medovik.jpg",
-      description: "Honey sponge layers with smooth sour cream filling",
-    },
-    {
-      id: 3,
-      name: "Red Velvet Cake",
-      price: 1500,
-      category: "Cakes",
-      image: "/images/red-velvet.jpg",
-      description: "Chocolate sponge with cream cheese frosting and berry sauce",
-    },
-  ]);
+  res.json(testProducts);
 });
 
 module.exports = router;
