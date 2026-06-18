fe05ba60cb879a44d907172458aa37bfbf20a843
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-06-18 18:42:28 +0200

    Create Product mongoose model

---
+++ backend/models/Product.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    flavor: {
+      type: String,
+      default: "",
+    },
+    ingredients: {
+      type: String,
+      default: "",
+    },
+    weight: {
+      type: Number,
+      default: 0,
+    },
+    height: {
+      type: Number,
+      default: 0,
+    },
+    diameter: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
