f916b322c56d8d03d246ddabb06212c76d691d08
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-06-26 17:49:28 +0200

    Add interactive review star rating

--- review_form/index.js
+++ review_form/index.js
@@ -1,1 +1,26 @@
-// Review form logic will be added later.
+const starButtons = document.querySelectorAll(".star-btn");
+
+let selectedRating = 5;
+
+function updateStarRating() {
+  starButtons.forEach((button) => {
+    const rating = Number(button.dataset.rating);
+
+    if (rating <= selectedRating) {
+      button.classList.add("is-active");
+    } else {
+      button.classList.remove("is-active");
+    }
+  });
+}
+
+starButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectedRating = Number(button.dataset.rating);
+    updateStarRating();
+  });
+});
+
+updateStarRating();
+
+// selectedRating will be used later when submitting the review.
