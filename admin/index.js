879342ba1681e177e7e505be551854b6b6857a77
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-06-29 10:44:53 +0200

    Use admin API helper for rejecting reviews

--- admin/index.js
+++ admin/index.js
@@ -127,7 +127,7 @@ rejectButton.addEventListener("click", async () => {
   }
 
   try {
-    const response = await fetch(
+    const response = await window.adminApiFetch(
       `http://localhost:3000/api/reviews/${pendingReview._id}/status`,
       {
         method: "PATCH",
